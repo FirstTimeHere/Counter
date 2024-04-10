75d7fcb8a588445db0aaddf8ce8c1a4f9127fd68
FirstTimeHere <97403208+FirstTimeHere@users.noreply.github.com>
2024-04-10 14:04:07 +0500

    Add files via upload

--- Counter/Assets/Scripts/Clicker.cs
+++ Counter/Assets/Scripts/Clicker.cs
@@ -4,41 +4,55 @@ using UnityEngine;
 
 public class Clicker : MonoBehaviour
 {
-    [SerializeField] private float _delay = 0.5f;
+    [SerializeField] private float _delay;
     [SerializeField] private TextMeshProUGUI _textNumber;
 
+    private Coroutine _coroutine;
+
     private int _number;
-    private static int s_mouseButton=0;
 
     private void Start()
     {
         _textNumber.text = _number.ToString();
-        StartCoroutine(RunCountDown());
+        Restart();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(s_mouseButton))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            StopCoroutine(CountDown());
-            DislpayCountDown(_number++);
+            Stop();
+            Restart();
         }
     }
 
-    private IEnumerator RunCountDown()
+    private void Stop()
     {
-        var wait = new WaitForSecondsRealtime(_delay);
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
+    }
+
+    private void Restart()
+    {
+        _coroutine = StartCoroutine(RunCountDown(_delay));
+    }
+
+    private IEnumerator RunCountDown(float delay)
+    {
+        var wait = new WaitForSecondsRealtime(delay);
 
         while (true)
         {
             _number++;
-            DislpayCountDown(_number);
+            DislpayCountDown(_number, _textNumber);
             yield return wait;
         }
     }
 
-    private void DislpayCountDown(float number)
+    private void DislpayCountDown(float number, TextMeshProUGUI text)
     {
-        _textNumber.text = number.ToString("");
+        text.text = number.ToString();
     }
 }
